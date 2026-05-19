097660a9be0a4a9a085e14650e8b62a4b03f4426
Yermakhankaspi <zhaksygali004@gmail.com>
2026-05-19 23:20:11 +0500

    Delete api directory

--- api/auth/me.js
+++
@@ -1,8 +0,0 @@
-// GET /api/auth/me
-import { getUser } from '../_lib/shared.js';
-
-export default async function handler(req, res) {
-  const user = await getUser(req);
-  if (!user) return res.status(401).json({ error: 'Not logged in' });
-  return res.status(200).json({ user: { email: user.email, name: user.name } });
-}
